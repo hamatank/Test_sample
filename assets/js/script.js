753950c542999a3b5161333aff3d503d28e26a1b
hamatank <sapsal7383@gmail.com>
2025-05-27 12:45:41 +0900

    work work
    work work responsive

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+  $(".navi").mouseover(function(){
+    $(".megamenu").addClass("active");
+  });
+  $(".navi").mouseleave(function(){
+    $(".megamenu").removeClass("active");
+    $(".megamenu-bg").removeClass("active");
+  });
+}); 
